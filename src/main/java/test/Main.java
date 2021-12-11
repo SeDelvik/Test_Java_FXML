2c0c0551a494995bc4dd3e9b88454dae2546e5df
Ilya Kudinkin <kudincikin@gmail.com>
2021-12-11 17:28:14 +0300

    test listview and class with things

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,23 +1,30 @@
 package test;
 
 import javafx.application.Application;
-import javafx.beans.property.StringProperty;
-import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
+    MrCl obj1 = new MrCl("1");
+    ObservableList<MrCl> data = FXCollections.observableArrayList(obj1);
+    //ListView<MrCl> listView = new ListView<>(data);
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Lil' pet");
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("TextClassBlyat.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("TextClassBlyat.fxml"));
         Parent mainView = loader.load(); //тут происходит загрузка вью и инициализация контроллера ( через метод initialize)
-        TextClassBlyat controller = loader.getController();
 
+        TextClassBlyat controller = loader.getController();
+        //controller.setText();
+        controller.setData(data);
+        data.add(new MrCl("2"));
 
         Scene scene = new Scene(mainView);
 
--- src/main/java/test/TextClassBlyat.java
+++ src/main/java/test/TextClassBlyat.java
@@ -1,9 +1,12 @@
 package test;
 
+import javafx.beans.Observable;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -12,22 +15,39 @@ import java.util.ResourceBundle;
 public class TextClassBlyat implements Initializable {
     @FXML
     private Text text1;
+    @FXML
+    private ListView<MrCl> listObj;
+    @FXML
+    private Text curObj;
 
     private StringProperty sheesh = new SimpleStringProperty();
+    private ObservableList<MrCl> list;
 
-
-    TextClassBlyat(){
+    public void setText(){
         sheesh.set("ora");
-        text1.textProperty().bind(this.sheesh);
     }
 
     public void changeText(){
         sheesh.set(sheesh.getValue()+"1");
     }
 
+    public void setData(ObservableList<MrCl> data){
+        list = data;
+        listObj.setItems(data);
+
+        listObj.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->{
+            if(newValue!=null) curObj.setText(newValue.toString());
+        });
+        listObj.getSelectionModel().selectFirst();
+    }
+    public void putNew(){
+        list.add(new MrCl("1"));
+    }
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        sheesh.set("ora");
+        setText();
         text1.textProperty().bind(this.sheesh);
     }
 }
